83b0a83029b80042febac1ca6bf9ff77b6c4694c
Stanislav <skinnystas@gmail.com>
2016-05-18 16:57:49 +0400

    added lookAhead method and new $ implementaion

--- dollar.java
+++ dollar.java
@@ -8,9 +8,49 @@ import java.util.Collections;
 import java.util.List;
 
 
-class Dollar {
-
+public class Dollar {
+        private final String tag = "DOLLAR";
+        
         public <T> T $(Object varName) {
+            try {
+                return (T) findField(getClass(), varName.toString()).get(this);
+            } catch (IllegalAccessException e) {
+                return null;
+            }
+        }
+
+        public <T> Class<? extends T> lookAhead(Object varName) {
+            Field field = findField(getClass(), varName.toString());
+            return field != null ? (Class<? extends T>) field.getType() : null;
+        }
+
+        private static Field findField(Class<?> current, String varName) {
+            try {
+                return current.getDeclaredField(varName);
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+            List<Class<?>> classesToGo = new ArrayList<>();
+            Class<?> sup = current.getSuperclass();
+            classesToGo.add(sup);
+            classesToGo.addAll(Arrays.asList(current.getInterfaces()));
+            for (Class<?> c : classesToGo) {
+                if (c != null && !c.equals(Object.class)) {
+                    try {
+                        Field f = findField(c, varName);
+                        if (f != null) {
+                            return f;
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+            return null;
+        }
+        
+        @Deprecated
+        public <T> T $_(Object varName) {
             try {
                 return (T) getClass().getDeclaredField(varName.toString()).get(this);
             } catch (Throwable t) {
@@ -21,31 +61,4 @@ class Dollar {
                 }
             }
         }
-        
-        private static Field findField(Class<?> current, String varName) {
-	        try {
-	            return current.getDeclaredField(varName);
-	        } catch (NoSuchFieldException e) {
-	            e.printStackTrace();
-	        }
-	        List<Class<?>> classesToGo = new ArrayList<>();
-	        Class<?> sup = current.getSuperclass();
-	        classesToGo.add(sup);
-	        classesToGo.addAll(Arrays.asList(current.getInterfaces()));
-	        for (Class<?> c : classesToGo) {
-	            if (c != null && !c.equals(Object.class)) {
-	                try {
-	                    Field f = findField(c, varName);
-	                    if (f != null) {
-	                        return f;
-	                    }
-	                } catch (Exception e) {
-	                    
-	                }
-	            }
-	        }
-	        return null;
-    	}
-
-        
     }
